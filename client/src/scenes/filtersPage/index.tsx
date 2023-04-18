641d232bfe34a000563c14341a8a0722976cf3a5
Юлия Мельниченко <julia_melni@MacBook-Air-Ulia.local>
2023-04-18 13:32:00 +0200

    wip: started create navbar

--- client/src/scenes/filtersPage/index.tsx
+++ client/src/scenes/filtersPage/index.tsx
@@ -1,5 +1,15 @@
+import Navbar from '../navbar';
+import { Box } from '@mui/material';
+
 const FiltersPage = () => {
-  return <div>FiltersPage</div>;
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Navbar />
+      <Box sx={{ width: '100%', padding: '2rem 6%', justifyContent: 'center' }}>
+        <Box sx={{ textAlign: 'center' }}>FILTERS PAGE</Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default FiltersPage;
--- client/src/scenes/homePage/index.tsx
+++ client/src/scenes/homePage/index.tsx
@@ -1,7 +1,15 @@
 import Navbar from '../navbar';
+import { Box } from '@mui/material';
 
 const HomePage = () => {
-  return <Navbar />;
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Navbar />
+      <Box sx={{ width: '100%', padding: '2rem 6%', justifyContent: 'center' }}>
+        <Box sx={{ textAlign: 'center' }}>HOME PAGE</Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default HomePage;
