49353a4f94802be2bc6e31c7015f1b6b90f82ced
Юлия Мельниченко <julia_melni@MacBook-Air-Ulia.local>
2023-04-19 12:41:27 +0200

    wip: added ChooseWebsite component

--- client/src/scenes/parser/index.tsx
+++ client/src/scenes/parser/index.tsx
@@ -1,11 +1,21 @@
-import { Box } from '@mui/material';
+import { Box} from '@mui/material';
+
+import { ChooseWebsite } from './ChooseWebsite';
 
 const Parser = () => {
-  return (
-    <Box sx={{ width: '100%', justifyContent: 'center', backgroundColor: 'lightblue' }}>
-      <Box sx={{ textAlign: 'center' }}>PARSER</Box>
-    </Box>
-  );
+  return <Box sx={{ width: '100%' }}>
+    <ChooseWebsite/>
+  </Box>;
 };
 
 export default Parser;
+
+// Choose parser website
+
+// Choose keywords
+
+// Choose city
+
+// Run parser + parsing status
+
+// Download results + marketing flow
