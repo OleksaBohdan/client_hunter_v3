d43d07309edb484d93b3556d45aebce02b5a299c
Юлия Мельниченко <julia_melni@MacBook-Air-Ulia.local>
2023-04-26 12:17:43 +0200

    feat: added Helmet component

--- client/src/scenes/homePage/index.tsx
+++ client/src/scenes/homePage/index.tsx
@@ -1,11 +1,19 @@
 import ResponsiveNavbar from '../navbar/NavigationBar';
 import { Box } from '@mui/material';
+import { Helmet } from 'react-helmet';
 
 const HomePage = () => {
   return (
-    <Box>
-      <ResponsiveNavbar />
-    </Box>
+    <>
+      <Helmet>
+        <meta name="description" content="Welcome to Clienthunter!" />
+        <title>Clienthunter-3 | home</title>
+      </Helmet>
+
+      <Box>
+        <ResponsiveNavbar />
+      </Box>
+    </>
   );
 };
 
--- client/src/scenes/loginPage/index.tsx
+++ client/src/scenes/loginPage/index.tsx
@@ -1,31 +1,38 @@
 import { Box } from '@mui/material';
 import Form from './Form';
 import logo from '../../assets/logo_full.png';
+import { Helmet } from 'react-helmet';
 
 const LoginPage = () => {
   return (
-    <Box sx={{ height: '100vh' }}>
-      <Box
-        sx={{
-          textAlign: 'center',
-          padding: '2rem',
-        }}
-      >
-        <img src={`${logo}?mw=164`} alt="logo" style={{ maxWidth: '250px' }} />
-      </Box>
+    <>
+      <Helmet>
+        <meta name="description" content="Web site created using create-react-app" />
+        <title>Clienthunter-3</title>
+      </Helmet>
+      <Box sx={{ height: '100vh' }}>
+        <Box
+          sx={{
+            textAlign: 'center',
+            padding: '2rem',
+          }}
+        >
+          <img src={`${logo}?mw=164`} alt="logo" style={{ maxWidth: '250px' }} />
+        </Box>
 
-      <Box
-        sx={{
-          padding: '2rem',
-          paddingTop: '4rem',
-          textAlign: 'center',
-          maxWidth: '700px',
-          m: '2rem auto',
-        }}
-      >
-        <Form />
+        <Box
+          sx={{
+            padding: '2rem',
+            paddingTop: '4rem',
+            textAlign: 'center',
+            maxWidth: '700px',
+            m: '2rem auto',
+          }}
+        >
+          <Form />
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
