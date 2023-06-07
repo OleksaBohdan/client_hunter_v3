860fdca21c85cdee72e96b3bd106c0c9ed918030
Bohdan Oleksa <bohdan.oleksa@gmail.com>
2023-06-07 18:56:04 +0200

    refactor

--- client/src/api/clientApi.ts
+++ client/src/api/clientApi.ts
@@ -1,4 +1,4 @@
-export const serverUrl = 'http://localhost:3001'
-export const serverWsUrl = 'ws://localhost:3001'
+export const serverUrl = process.env.SERVER_HTTPS_URL || 'http://localhost:3001';
+export const serverWsUrl = process.env.SERVER_WSS_URL || 'ws://localhost:3001';
 // export const serverUrl = 'https://ch-backend-smzhcuhsia-ey.a.run.app/'
 // export const serverWsUrl = 'wss://ch-backend-smzhcuhsia-ey.a.run.app/'
